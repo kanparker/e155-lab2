bac74b12532dd50afdf2835c9facdd5614700493
Kanoa Parker <kanparker@hmc.edu>
2025-09-07 19:08:11 -0700

    bin files

--- src/e155_lab2/impl_1/e155_lab2_impl_1_map.tcl
+++ src/e155_lab2/impl_1/e155_lab2_impl_1_map.tcl
@@ -4,15 +4,16 @@
 set ret 0
 if {[catch {
 
-if {![file exists {C:/Users/kanparker/Downloads/e155_lab2/src/e155_lab2/impl_1}]} {
-  file mkdir {C:/Users/kanparker/Downloads/e155_lab2/src/e155_lab2/impl_1}
+if {![file exists {C:/Users/kanparker/Downloads/e155-lab2/src/e155_lab2/impl_1}]} {
+  file mkdir {C:/Users/kanparker/Downloads/e155-lab2/src/e155_lab2/impl_1}
 }
-cd {C:/Users/kanparker/Downloads/e155_lab2/src/e155_lab2/impl_1}
+cd {C:/Users/kanparker/Downloads/e155-lab2/src/e155_lab2/impl_1}
 
-sys_set_attribute -gui on -msg {C:/Users/kanparker/Downloads/e155_lab2/src/e155_lab2/promote.xml}
-msg_load {C:/Users/kanparker/Downloads/e155_lab2/src/e155_lab2/promote.xml}
+sys_set_attribute -gui on -msg {C:/Users/kanparker/Downloads/e155-lab2/src/e155_lab2/promote.xml}
+msg_load {C:/Users/kanparker/Downloads/e155-lab2/src/e155_lab2/promote.xml}
 des_set_project_udb -in {e155_lab2_impl_1_syn.udb} -out {e155_lab2_impl_1_map.udb} -milestone map -pm ice40tp
 des_set_reference_udb -clean
+map_set_option {pdc_file "C:/Users/kanparker/Downloads/e155-lab2/src/e155_lab2/e155_lab2.pdc"}
 # map option
 map_set_option { report_symbol_cross_reference false report_signal_cross_reference false   ignore_constraint_errors false}
 map_run
--- src/e155_lab2/impl_1/e155_lab2_impl_1_synthesize.tcl
+++ src/e155_lab2/impl_1/e155_lab2_impl_1_synthesize.tcl
@@ -5,16 +5,14 @@ source [file join {C:/lscc/radiant/2024.2} scripts tcl flow run_engine.tcl]
 # define global variables
 global para
 set para(gui_mode) "1"
-set para(prj_dir) "C:/Users/kanparker/Downloads/e155_lab2/src/e155_lab2"
-if {![file exists {C:/Users/kanparker/Downloads/e155_lab2/src/e155_lab2/impl_1}]} {
-  file mkdir {C:/Users/kanparker/Downloads/e155_lab2/src/e155_lab2/impl_1}
+set para(prj_dir) "C:/Users/kanparker/Downloads/e155-lab2/src/e155_lab2"
+if {![file exists {C:/Users/kanparker/Downloads/e155-lab2/src/e155_lab2/impl_1}]} {
+  file mkdir {C:/Users/kanparker/Downloads/e155-lab2/src/e155_lab2/impl_1}
 }
-cd {C:/Users/kanparker/Downloads/e155_lab2/src/e155_lab2/impl_1}
+cd {C:/Users/kanparker/Downloads/e155-lab2/src/e155_lab2/impl_1}
 # synthesize IPs
 # synthesize VMs
 # synthesize top design
-file delete -force -- e155_lab2_impl_1.vm e155_lab2_impl_1.ldc
-::radiant::runengine::run_engine_newmsg synthesis -f "C:/Users/kanparker/Downloads/e155_lab2/src/e155_lab2/impl_1/e155_lab2_impl_1_lattice.synproj" -logfile "e155_lab2_impl_1_lattice.srp"
 ::radiant::runengine::run_postsyn [list -a iCE40UP -p iCE40UP5K -t SG48 -sp High-Performance_1.2V -oc Industrial -top -w -o e155_lab2_impl_1_syn.udb e155_lab2_impl_1.vm] [list e155_lab2_impl_1.ldc]
 
 } out]} {
